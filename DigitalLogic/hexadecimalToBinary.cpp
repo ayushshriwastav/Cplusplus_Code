3644c14423fd7962b73a3c560459a56144629964
ayushshriwastav <shriwastavayush22@gmail.com>
2026-02-19 16:54:46 +0530

    hexadecimal to binary logic

--- DigitalLogic/hexadecimalToBinary.cpp
+++ DigitalLogic/hexadecimalToBinary.cpp
@@ -36,3 +36,43 @@ int main(){
     return 0;
     
 }
+
+
+
+///// We can also use the following code to convert hexadecimal to binary without using string manipulation using switch and case statement 
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    string hex;
+    cout << "Enter hexadecimal number: ";
+    cin >> hex;
+
+    string binary = "";
+
+    for (int i = 0; i < hex.length(); i++) {
+
+        switch (hex[i]) {
+            case '0': binary += "0000"; break;
+            case '1': binary += "0001"; break;
+            case '2': binary += "0010"; break;
+            case '3': binary += "0011"; break;
+            case '4': binary += "0100"; break;
+            case '5': binary += "0101"; break;
+            case '6': binary += "0110"; break;
+            case '7': binary += "0111"; break;
+            case '8': binary += "1000"; break;
+            case '9': binary += "1001"; break;
+            case 'A': case 'a': binary += "1010"; break;
+            case 'B': case 'b': binary += "1011"; break;
+            case 'C': case 'c': binary += "1100"; break;
+            case 'D': case 'd': binary += "1101"; break;
+            case 'E': case 'e': binary += "1110"; break;
+            case 'F': case 'f': binary += "1111"; break;
+        }
+    }
+
+    cout << "Binary number is: " << binary << endl;
+}
+*/
