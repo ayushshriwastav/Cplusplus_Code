fe01f5fdce59cbf21aaea804e77f2a7dfc9f05f7
ayushshriwastav <shriwastavayush22@gmail.com>
2026-09-14 12:45:00 +0530

    permutation of string using character array and string function

--- recursion/permutationOfString.cpp
+++ recursion/permutationOfString.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <string>
 using namespace std;
 
@@ -49,6 +49,90 @@ int main()
     }
 
     permutations(str, "",r);
+
+    return 0;
+}
+*/
+
+
+// Permutation of String using character array by recursion
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void permutationOfString(char str[], char result[], int index = 0, int r =1){
+
+    // Base Condition
+    if (index == r){
+
+        cout << "[";
+    for(int i = 0; i< r;i++){
+        cout << result[i];
+    }
+    
+        cout << "]" << endl;
+        return;
+    }
+
+    // chose the each character one by one and find the remaining string
+    for (int i = 0; str[i]!= '\0'; i++){
+
+        char current_string = str[i];
+
+
+        char remaining_string[100];
+        int k = 0;
+
+        for (int j = 0; str[j] != '\0'; j++){
+
+            if (j != i){
+                remaining_string[k++] = str[j];
+            }
+        }
+
+        remaining_string[k] = '\0'; // Null-terminate the remaining string  
+
+        // add current character to the result and find the permutations of the remaining string
+        result[index] = current_string;
+        permutationOfString(remaining_string, result, index + 1, r);
+    }
+};
+
+int main(){
+
+    //const int MAX = 100;
+    
+    int n;
+    cout << "Enter the length of the string: ";
+    cin >> n;
+
+    if (n <= 0 || n > n+1){
+        cout << "Invalid string length" << endl;
+        return 0;
+    }
+
+    char str[n+1];
+    cout << "Enter a string: "; 
+
+    for (int i = 0; i < n; i++){
+
+        cin >> str[i];
+    }
+
+    str[n] = '\0'; // Null-terminate the string
+
+    int r;
+    cout << "Enter the length of the permutation: ";
+    cin >> r;
+
+    if(r > strlen(str) || r <= 0){
+        cout << "Invalid length of permutation" << endl;
+        return 0;
+    }
+
+    char result[100];
+    permutationOfString(str, result, 0, r);
 
     return 0;
 }
