4a86f977a026ceb7cbd2bb4edbd32703fb10e357
ayushshriwastav <shriwastavayush22@gmail.com>
2026-09-14 12:39:23 +0530

    Substring of String by Character array and String function

--- recursion/substringOfString.cpp
+++ recursion/substringOfString.cpp
@@ -11,6 +11,70 @@ int mystrlen(const char str[]){
     return count;
 }
 
+int printSubstrings(string str, int start, int end)
+{
+    // Base condition
+    if (start == str.length())
+    {
+        return 0;
+    }
+
+    // under end string length
+    if (end < str.length())
+    {
+        cout << "[";
+        // print the start and end index of the string
+        for (int i = start; i <= end; i++)
+        {
+            cout << str[i];
+
+            if(i < end){
+                cout << ",";
+            }
+        }
+        cout << "]" << endl;
+
+        // increase the end index to print the next substring
+        return 1 + printSubstrings(str, start, end + 1);
+    }
+    else
+    {
+        // move to the next starting position
+        return printSubstrings(str, start + 1, start + 1);
+    }
+};
+
+int main()
+{
+   string str;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    int totalSubsting = printSubstrings(str, 0, 0);
+
+    cout << "Total number of substrings: " << totalSubsting << endl;
+
+    return 0;
+}
+
+
+
+
+/*
+#include <iostream>
+using namespace std;
+
+
+int mystrlen(const char str[]){
+    int count = 0;
+
+    while(str[count] != '\0'){
+        count++;
+    }
+    return count;
+}
+
 int printSubstrings(char str[], int start, int end)
 {
     // Base condition
@@ -71,3 +135,5 @@ int main()
 
     return 0;
 }
+
+*/
